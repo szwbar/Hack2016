b9041991ea90e206d5fd4dd3f68974d488cad352
BarS <szwbar@gmail.com>
2016-05-19 14:11:57 +0300

    First integration

---
+++ app/src/main/java/OpeningScreens/LogInActivity.java
@@ -0,0 +1,21 @@
+package OpeningScreens;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+import com.example.szwba_000.killers.R;
+
+/**
+ * Created by szwba_000 on 19/05/2016.
+ */
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/example/szwba_000/killers/MainActivity.java
+++ app/src/main/java/com/example/szwba_000/killers/MainActivity.java
@@ -2,12 +2,47 @@ package com.example.szwba_000.killers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        signInButton = (Button) findViewById(R.id.activity_login_enter_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userName = (EditText) findViewById(R.id.activity_login_enter_username);
+                EditText password = (EditText) findViewById(R.id.activity_login_enter_password);
+                if (userName == null || password == null) {
+                    // TODO CHECK
+                } else {
+                    String userNameTxt = userName.getText().toString();
+                    String passwordTxt = password.getText().toString();
+                    if (!userNameTxt.matches("[0-9]{9}")) { // Password is not in ID form
+                        userName.setText("");
+                        return;
+                    }
+
+                    if (approveUser(userNameTxt, passwordTxt)) {
+
+
+                    } else {
+                        // TODO USER NOT RECOGNIZED, POP UP
+                        userName.setText("");
+                        password.setText("");
+                    }
+                }
+
+            }
+        });
     }
+
 }
