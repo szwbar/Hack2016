ab276cf56539d03cfe56f4d3d4d8ada86369553b
BarS <szwbar@gmail.com>
2016-05-19 14:15:32 +0300

    Second first attempt

--- app/src/main/java/OpeningScreens/LogInActivity.java
+++ app/src/main/java/OpeningScreens/LogInActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.szwba_000.killers.R;
 
@@ -13,9 +16,40 @@ import com.example.szwba_000.killers.R;
  */
 public class LogInActivity extends AppCompatActivity {
 
+    private Button signInButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+        signInButton = (Button) findViewById(R.id.activity_login_enter_button);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText userName = (EditText) findViewById(R.id.activity_login_enter_username);
+                EditText password = (EditText) findViewById(R.id.activity_login_enter_password);
+                if (userName == null || password == null) {
+                    // TODO CHECK
+                } else {
+                    String userNameTxt = userName.getText().toString();
+                    String passwordTxt = password.getText().toString();
+                    if (!userNameTxt.matches("[0-9]{9}")) { // Password is not in ID form
+                        userName.setText("");
+                        return;
+                    }
+
+                    if (approveUser(userNameTxt, passwordTxt)) {
+
+
+                    } else {
+                        // TODO USER NOT RECOGNIZED, POP UP
+                        userName.setText("");
+                        password.setText("");
+                    }
+                }
+
+            }
+        });
     }
+
 }
